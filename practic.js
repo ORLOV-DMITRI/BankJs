135bf4d9b35e497fe5f65e1d4ef7a289e2269fc7
Dmitri Orlov <dmitri.anatolevich.orlov@gmail.com>
2023-05-07 13:31:22 +0600

    fg

--- practic.js
+++ practic.js
@@ -1,22 +1,38 @@
 const cats = [
-  { catWeight: 3, foodWeight: 20, owners: ["Наташа"] },
-  { catWeight: 6, foodWeight: 90, owners: ["Марина", "Алиса"] },
-  { catWeight: 4, foodWeight: 45, owners: ["Алекс", "Ирина"] },
+  { catWeight: 3, foodWeight: 25, owners: ["Наташа"] },
+  { catWeight: 8, foodWeight: 90, owners: ["Марина", "Алиса"] },
+  { catWeight: 4, foodWeight: 30, owners: ["Алекс", "Ирина"] },
   { catWeight: 7, foodWeight: 80, owners: ["Борис"] },
 ];
 
 const reсommendedPortion = (catWeight) => catWeight * 0.75 * 12;
-const recommendedPortionFormula = (cat) => {};
+const recommendedPortionFormula = (cat) => {
+  if (
+    cat.foodWeight > cat.reсommendedPortion * 0.9 &&
+    cat.foodWeight < cat.reсommendedPortion * 1.1
+  ) {
+    return 'нормально'
+  }
+  else if (cat.foodWeight > cat.reсommendedPortion) {
+    return 'много'
+  } else if (cat.foodWeight < cat.reсommendedPortion) {
+    return 'мало'
+  }
+};
 
+// 1
 cats.forEach((cat) => {
   cat.reсommendedPortion = reсommendedPortion(cat.catWeight);
 });
 console.log(cats);
 
-const ownersCats = cats.flatMap((cat) => cat.owners);
-console.log(ownersCats);
+// 2 
+const findCatsByOwner = function (name) {
+  cats.forEach((cat) => {
+    if(cat.owners.includes(name)){
+      console.log(`Кот владельца ${name}, ест ${recommendedPortionFormula(cat)}`)
+    }
+  });
+};
 
-cats.forEach((cat) => {
-  console.log(recommendedPortionFormula(cat));
-  // recommendedPortionFormula(cat);
-});
+findCatsByOwner('Алекс');
